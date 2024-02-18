7c8ca229c77eb42a93f3774422ff8790043d33ee
JheyBi <joaodlrio@outlook.com>
2024-02-18 18:01:54 -0300

    Atualizacao LivroController: buscarPorID, verificaLivro

--- src/main/java/com/joao/biblioteca/controller/LivroController.java
+++ src/main/java/com/joao/biblioteca/controller/LivroController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,13 +43,19 @@ public class LivroController {
         return ResponseEntity.ok(repository.findByIsbn(isbn));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> buscarPorId(@PathVariable(value = "id")Long id) {
+        return ResponseEntity.ok(repository.findById(id));
+    }
+
+
     // Metodos de negocio
-    public Boolean verificaLivro(String isbn){
+    public Boolean verificaLivro(Long id){
         //Verifica se o livro já está cadastrado
-        if (repository.findByIsbn(isbn) == null) {
+        if (repository.findById(id) == null) {
             return false;
         }
-        Livro livro = repository.findByIsbn(isbn);
+        Livro livro = repository.findById(id);
         //Verifica se o livro está disponivel
         if(livro.getDisponivel() == false){
             return false;
