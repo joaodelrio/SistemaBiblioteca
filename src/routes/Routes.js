b9cb4f3615fb69c67b9a513bc26231d7ac47068c
JheyBi <joaodlrio@outlook.com>
2024-02-20 21:43:19 -0300

    update

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,14 +2,14 @@ import React, { Fragment } from "react";
 import { BrowserRouter, Route, Routes as RouterRoutes } from "react-router-dom";
 import Alunos from "../pages/Aluno";
 import Livros from "../pages/Livro";
-import App from "../pages/App";
+import Home from "../pages/Home";
 
 const Routes = () => {
     return (
       <BrowserRouter>
         <Fragment>
           <RouterRoutes>
-              <Route path="/" element={<App />} />
+              <Route path="/" element={<Home />} />
               <Route path="/alunos" element={<Alunos />} />
               <Route path="/livros" element={<Livros />} />
           </RouterRoutes>
